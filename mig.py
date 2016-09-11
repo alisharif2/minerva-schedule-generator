af78a85c2f4fd62144b6250e73adf705176e3382
Ali <ali.sharif2@mail.mcgill.ca>
2016-09-10 21:55:53 -0400

    moved argument parsing code into mig.py and created sched_html_reader.py to parse html data

---
+++ mig.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+import requests, os, sys, argparse, getpass
+from login import login
+from sched_html_reader import extract_schedule
+
+# Command line argument check
+parser = argparse.ArgumentParser(description='Login to minerva and download personal schedule page in html')
+parser.add_argument('userid', action='store')
+parser.add_argument('-f', '--fall', action='store_true', default=False, dest='fall')
+parser.add_argument('-w', '--winter', action='store_true', default=False, dest='winter')
+parser.add_argument('year', action='store', type=int)
+parser.add_argument('-p', '--password', action='store', dest='user_pass')
+
+args = parser.parse_args()
+
+# validate the semester argument
+# -f and -w are much easier to write than 'fall' or 'winter'
+# unfortunately that means I have to deal with them being optional
+# TODO find a way to make the semester argument exclusive or
+if args.fall and args.winter:
+    print("Please enter either 'fall' or 'winter' not both")
+if not args.fall and not args.winter:
+    print("Please pass a semester")
+
+# Collect user's password without echo
+if args.user_pass is None:
+    user_pass = getpass.getpass()
+else:
+    user_pass = args.user_pass
+
+# calculate the 'term_in' variable for the target POST
+term = 0
+if args.fall:
+    term = args.year * 100 + 9
+    semester = 'fall'
+elif args.winter:
+    term = args.year * 100 + 1
+    semester = 'winter'
+
+raw_html = login(args.userid, user_pass, term, semester, args.year)
+course_data = extract_schedule(raw_html)
---
+++ sched_html_reader.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+from bs4 import BeautifulSoup as bsoup
+from collections import defaultdict
+
+def extract_schedule(raw_html):
+    clean_html = bsoup(raw_html, 'html.parser')
+    info = []
+    times = []
+    for tag in clean_html.find_all("table", class_="datadisplaytable"):
+        if "Scheduled Meeting Times" in tag.caption.get_text():
+            times.append(tag)
+        else:
+            info.append(tag)
+
+    tables = zip(info, times)
+    course_data_raw = defaultdict(dict)
+    for infobox, meeting_times in tables:
+        class_title = infobox.caption.get_text()
+        for title_box, value_box in zip(list(meeting_times.find_all('th')), list(meeting_times.find_all('td'))):
+            course_data_raw[class_title][title_box.string] = value_box.string
+
+    return course_data_raw
